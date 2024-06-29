c3e0935aaff883cdf3c9c3affa82bedf38235eeb
Bharath Kesari <130210092+bharathcoder7350@users.noreply.github.com>
2024-06-29 13:20:39 +0530

    Update App.jsx

--- Todo-app_Frontend/Todo-app/src/App.jsx
+++ Todo-app_Frontend/Todo-app/src/App.jsx
@@ -3,8 +3,16 @@ import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
 
 function App(){
+  
+  
   const {todos,setTodos} = useState([])
 
+  useEffect(async()=>{
+    const response = await fetch('http://localhost:3000/todos');
+    const data = await response.json()
+    setTodos(data)
+  }, [])
+  
   return <div>
     <CreateTodo></CreateTodo>
     <Todos todos={todos}></Todos>
